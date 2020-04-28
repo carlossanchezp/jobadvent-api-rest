658319eaa864d7596ffe90826bd5d9c205f649cc
Carlos Sánchez Pérez <carloscsanchezperez@gmail.com>
2020-04-28 17:20:01 +0200

    Add protected show user callback

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
-  before_action :check_owner, only: %i[update destroy]
+  before_action :check_owner, only: %i[show update destroy]
 
   # GET /users/1
   def show
