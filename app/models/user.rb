65b378d538a318f363793cffe570181b0b9f3e61
Carlos Sánchez Pérez <carloscsanchezperez@gmail.com>
2020-04-28 19:53:00 +0200

    Add products crud and testing and seed user and products

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # Secutity
   has_secure_password
   
+  # Relations
+  has_many :products, dependent: :destroy
+
+  # Validations
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
